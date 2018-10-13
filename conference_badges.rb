ce62870908946c0cd39c2aaad2decc2181765097
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-13 18:00:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ end
 
 def batch_badge_creator(array)
   new_badge = []
-  array.each {|name| new_badge.push("Hello, my name is #{name}.")
+  array.each {|name| new_badge.push("Hello, my name is #{name}.")}
   return new_badge
 end
